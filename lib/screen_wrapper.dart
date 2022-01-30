4b25294ddd51ab69f18b199e10dc055a59898010
Jan Vaverka <jan.vaverka@asnplus.cz>
2022-01-30 19:23:38 +0100

    Removed chart screen

--- lib/screen_wrapper.dart
+++ lib/screen_wrapper.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: implementation_imports
 
 import 'package:analyzer/providers/navigation_provider.dart';
-import 'package:analyzer/screens/chart_screen.dart';
 import 'package:analyzer/screens/home_screen.dart';
 import 'package:analyzer/screens/settings_screen.dart';
 import 'package:analyzer/theme/colors.dart';
@@ -16,7 +15,6 @@ class ScreenWrapper extends StatelessWidget {
   Widget build(BuildContext context) {
     final screens = [
       const HomeScreen(),
-      const ChartScreen(),
       const SettingsScreen(),
     ];
     return Scaffold(
@@ -25,9 +23,9 @@ class ScreenWrapper extends StatelessWidget {
         onTap: (index) => context.read<NavigationProvider>().setScreenIndex(index),
         currentIndex: context.watch<NavigationProvider>().screenIndex,
         backgroundColor: surfaceColor,
+        fixedColor: primaryColor,
         items: const [
           BottomNavigationBarItem(label: "Home", icon: Icon(FontAwesomeIcons.home)),
-          BottomNavigationBarItem(label: "OTDR", icon: Icon(FontAwesomeIcons.chartLine)),
           BottomNavigationBarItem(label: "Settings", icon: Icon(FontAwesomeIcons.userCog)),
         ],
       ),
