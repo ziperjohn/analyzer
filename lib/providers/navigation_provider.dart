517f0f1333967ac63fd329e320c92d3855c410be
Jan Vaverka <jan.vaverka@asnplus.cz>
2022-02-01 19:12:56 +0100

    Changed with to extends

--- lib/providers/navigation_provider.dart
+++ lib/providers/navigation_provider.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class NavigationProvider with ChangeNotifier {
+class NavigationProvider extends ChangeNotifier {
   int _screenIndex = 0;
 
   int get screenIndex => _screenIndex;
