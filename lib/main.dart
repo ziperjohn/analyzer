15f4c6aaa94f1bcc6bf804e74b5f09a25ff274f0
Jan Vaverka <jan.vaverka@asnplus.cz>
2022-02-03 18:13:59 +0100

    Added Stream provider to material app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:analyzer_app/models/analyzer_model.dart';
 import 'package:analyzer_app/providers/analyzer_provider.dart';
 import 'package:analyzer_app/providers/navigation_provider.dart';
+import 'package:analyzer_app/services/firestore_service.dart';
 import 'package:analyzer_app/theme/theme.dart';
 import 'package:analyzer_app/utils/routes.dart';
 import 'package:flutter/material.dart';
@@ -7,15 +9,7 @@ import 'package:provider/provider.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => NavigationProvider()),
-      ],
-      child: const Main(),
-    ),
-  );
+  runApp(const Main());
 }
 
 class Main extends StatelessWidget {
@@ -23,12 +17,18 @@ class Main extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "Analyzer",
-      routes: appRoutes,
-      darkTheme: darkMode,
-      themeMode: ThemeMode.dark,
+    return MultiProvider(
+      providers: [
+         ChangeNotifierProvider(create: (_) => NavigationProvider()),
+         StreamProvider(create: (_) => FirestoreService().analyzers , initialData: Analyzer())
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: "Analyzer",
+        routes: appRoutes,
+        darkTheme: darkMode,
+        themeMode: ThemeMode.dark,
+      ),
     );
   }
 }
