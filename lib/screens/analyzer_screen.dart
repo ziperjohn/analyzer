80962c1555b615e833df6dec058bc67dd7441be9
Jan Vaverka <jan.vaverka@asnplus.cz>
2022-02-04 14:55:03 +0100

    changed name of device widget and import to analyzer screen

--- lib/screens/analyzer_screen.dart
+++ lib/screens/analyzer_screen.dart
@@ -1,4 +1,6 @@
 import 'package:analyzer_app/models/analyzer_model.dart';
+import 'package:analyzer_app/widgets/analyzer_device.dart';
+import 'package:analyzer_app/widgets/main_chart.dart';
 import 'package:flutter/material.dart';
 
 class AnalyzerScreen extends StatelessWidget {
@@ -12,6 +14,15 @@ class AnalyzerScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(analyzer.name),
         centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            const AnalyzerDevice(),
+            MainChart(),
+          ],
+        ),
       ),
     );
   }
